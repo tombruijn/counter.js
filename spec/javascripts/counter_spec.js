0abe9645e26f898574b474234e04fcf50e13e41e
Tom de Bruijn <tom@tomdebruijn.com>
2013-08-04 20:47:08 +0200

    Spec the options and their defaults

--- spec/javascripts/counter_spec.js
+++ spec/javascripts/counter_spec.js
@@ -17,6 +17,60 @@ describe("Counter.js", function() {
       element.counter("start");
       expect(counter.running).toBe(true);
     });
+
+    it("should merge set options", function(){
+      var onStart = function() { };
+      var onComplete = function() { };
+      var element = $("<div>");
+      element.counter({
+        autoStart: false,
+        duration: 10,
+        countFrom: 1,
+        countTo: 2,
+        placeholder: "?",
+        easing: "linear",
+        onStart: onStart,
+        onComplete: onComplete
+      });
+      options = element.data("plugin_counter").options
+      expect(options.autoStart).toBe(false);
+      expect(options.duration).toBe(10);
+      expect(options.countFrom).toBe(1);
+      expect(options.countTo).toBe(2);
+      expect(options.placeholder).toBe("?");
+      expect(options.easing).toBe("linear");
+      expect(options.onStart).toBe(onStart);
+      expect(options.onComplete).toBe(onComplete);
+    });
+
+    it("should set defaults for undefined options", function(){
+      var element = $("<div>");
+      element.counter({ countFrom: 0, countTo: 1 });
+      options = element.data("plugin_counter").options
+      expect(options.autoStart).toBe(true);
+      expect(options.duration).toBe(1500);
+      expect(options.countFrom).toBe(0);
+      expect(options.countTo).toBe(1);
+      expect(options.easing).toBe("easeOutQuad");
+    });
+
+    describe("element value", function(){
+      it("should use the element value for countTo if larger than countFrom", function(){
+        var element = $("<div>1</div>");
+        element.counter({ countFrom: 0 });
+        options = element.data("plugin_counter").options
+        expect(options.countFrom).toBe(0);
+        expect(options.countTo).toBe(1);
+      });
+
+      it("should use the element value for countFrom if larger than countTo", function(){
+        var element = $("<div>1</div>");
+        element.counter({ countTo: 2 });
+        options = element.data("plugin_counter").options
+        expect(options.countFrom).toBe(1);
+        expect(options.countTo).toBe(2);
+      });
+    });
   });
 
   describe("setNumber()", function(){
